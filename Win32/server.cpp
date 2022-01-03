34885da21691da92f5964a69231121d6a6025568
Lương Văn Tú <luongvantuit@gmail.com>
2022-01-03 08:06:59 +0700

    comment code server.cpp

--- Win32/server.cpp
+++ Win32/server.cpp
@@ -3,25 +3,28 @@
 
 int main(int argc, char const *argv[])
 {
+
+    // Initialization variables
     WSADATA wsaData;
     SOCKADDR_IN serAddr, cliAddr;
     int iRc;
     int adsize;
     char buffer[256];
 
+    // Initialization socket server
     iRc = WSAStartup(MAKEWORD(2, 2), &wsaData);
     SOCKET ssk = socket(AF_INET, SOCK_STREAM, 0);
     serAddr.sin_family = AF_INET;
     serAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serAddr.sin_port = htons(1024);
 
+    // Bind & listen accept connect
     iRc = bind(ssk, (LPSOCKADDR)&serAddr, sizeof(serAddr));
     iRc = listen(ssk, 5);
-
     adsize = sizeof(cliAddr);
-
     SOCKET sk = accept(ssk, (LPSOCKADDR)&cliAddr, &adsize);
 
+    // Receive data from client
     iRc = recv(sk, buffer, 256, 0);
     if (iRc == SOCKET_ERROR)
         printf("Cound not receive data from server.\n");
