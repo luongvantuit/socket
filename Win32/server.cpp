dd0c7e8a3b345533fd6ba216542a121f5069254d
Lương Văn Tú <luongvantuit@gmail.com>
2022-01-03 07:57:55 +0700

    socket server

--- Win32/server.cpp
+++ Win32/server.cpp
@@ -1,6 +1,40 @@
-#include<winsock2.h>
+#include <winsock2.h>
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
+    WSADATA wsaData;
+    SOCKADDR_IN serAddr, cliAddr;
+    int iRc;
+    int adsize;
+    char buffer[256];
+
+    iRc = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    SOCKET ssk = socket(AF_INET, SOCK_STREAM, 0);
+    serAddr.sin_family = AF_INET;
+    serAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serAddr.sin_port = htons(1024);
+
+    iRc = bind(ssk, (LPSOCKADDR)&serAddr, sizeof(serAddr));
+    iRc = listen(ssk, 5);
+
+    adsize = sizeof(cliAddr);
+
+    SOCKET sk = accept(ssk, (LPSOCKADDR)&cliAddr, &adsize);
+
+    iRc = recv(sk, buffer, 256, 0);
+    if (iRc == SOCKET_ERROR)
+        printf("Cound not receive data from server.\n");
+    else
+    {
+        buffer[iRc] = 0;
+        printf("Message from client: %s\n",buffer);
+    }
+
+    // Close socket clear assets
+    closesocket(sk);
+    closesocket(ssk);
+    WSACleanup();
+
     return 0;
 }
